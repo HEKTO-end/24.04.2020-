4bd8eec0bdf809e85eb720b98856d12c9d47fdfa
HEKTO-end <54909485+HEKTO-end@users.noreply.github.com>
2020-04-24 10:54:17 +0400

    Добавлен проект
    без изменений

---
+++ Координаты/Point.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Координаты
+{
+    class Point
+    {
+        public int x, y; 
+        public Point() { x = 0; y = 0; }
+        public Point(int x, int y) { this.x = x; this.y = y; }
+        public override string ToString() => "x=" + x + " y=" + y;
+        public double Distance() => Math.Sqrt(x * x + y * y);
+        public void Move(int a, int b) 
+        {
+            x += a;
+            y += b;
+        }
+        public int X
+        {
+            get 
+            { 
+                return x; 
+            }
+            set 
+            { 
+                x = value; 
+            }
+        }
+        public int Y
+        {
+            get 
+            { 
+                return y; 
+            }
+            set 
+            { 
+                y = value; 
+            }
+        }
+        public int Scalar
+        {
+            set
+            {
+                x = x * value;
+                y = y * value;
+            }
+        }
+    }
+}
---
+++ Координаты/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Координаты
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point();
+            Console.WriteLine(a.ToString()); 
+            Console.ReadLine();
+            
+        }
+    }
+}
